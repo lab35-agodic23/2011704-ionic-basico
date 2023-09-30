494ff0b3afca657167d0cb74ff7fc7affaf5b9a3
AngelF4 <angelfhernandez71@gmail.com>
2023-09-30 13:39:10 -0600

    Practica 7

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'miApp-2011704',
+  appName: 'miApp-2011704a',
   webDir: 'www',
   server: {
     androidScheme: 'https'
