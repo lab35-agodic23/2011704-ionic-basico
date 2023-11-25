f5665f2a9a7234919cc2f2c8ea8cdc0d56b6ab74
AngelF4 <angelfhernandez71@gmail.com>
2023-11-25 12:08:38 -0600

    practicas 12 y 13

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -2,14 +2,16 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
-
+import { Platform } from '@ionic/angular';
 @Injectable({
   providedIn: 'root'
 })
 
 export class PhotoService {
 
-  constructor() { }
+  constructor(plataforma: Platform) { 
+    plataforma =plataforma;
+  }
 
   public async addNewToGallery() {
     // Take a photo
